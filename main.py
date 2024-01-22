c6f738f94bf784c3bf613d4517f49b7c9d754aab
vian96 <vianmart@yandex.ru>
2024-01-22 03:43:40 +0300

    first fully working engine

--- main.py
+++ main.py
@@ -16,11 +16,13 @@ def main():
     cys = np.array([0, 1, 1, 0,-1,-1,-1, 0, 1])
     weights = np.array([4/9,1/9,1/36,1/9,1/36,1/9,1/36,1/9,1/36])
 
-    F = np.ones((Ny, Nx, Nl)) + 0.01 * np.random.randn(Ny, Nx, Nl)
+    F = np.ones((Ny, Nx, Nl)) + 0.1 * np.random.randn(Ny, Nx, Nl)
     F[:,:,3] = 2.3
 
     X, Y = np.meshgrid(range(Nx), range(Ny))
-    cyclinder = (X - Nx/4)**2 + (Y - Ny/2)**2 < 13**2
+    cyclinder = (X - Nx/2)**2 + (Y - Ny/2)**2 < 13**2
+    engine = (abs(X-Nx/4) < Nx/20) & (abs(Y - Ny/2) < Ny/8)
+
 
     plt.figure(figsize=(16,8), dpi=80)
 
@@ -35,16 +37,17 @@ def main():
             F[:,:,i] = np.roll(F[:,:,i], cx, axis=1)
             F[:,:,i] = np.roll(F[:,:,i], cy, axis=0)
 
+        rho = np.sum(F, 2)
+        ux  = np.clip(np.sum(F*cxs, 2) / rho, -0.4, 0.4)
+        uy  = np.clip(np.sum(F*cys, 2) / rho, -0.4, 0.4)
+
         bnry = F[cyclinder, :]
         bnry = bnry[:, [0,5,6,7,8,1,2,3,4]]
-        
-        rho = np.sum(F, 2)
-        ux  = np.sum(F*cxs, 2) / rho
-        uy  = np.sum(F*cys, 2) / rho
-        # print(F.shape, cyclinder.shape, F.shape)
         F[cyclinder, :] = bnry
         ux[cyclinder] = 0
         uy[cyclinder] = 0
+
+        ux[engine] = 0.25
 
         Feq = np.zeros(F.shape)
         for i, cx, cy, w in zip(range(Nl), cxs, cys, weights):
@@ -54,9 +57,11 @@ def main():
 
         F += -1/Tau * (F-Feq)
 
-        if it%100 == 0:
+        if it%10 == 0:
             print(it)
-            plt.imshow(np.sqrt(ux**2+uy**2))
+            v = np.clip(np.sqrt(ux**2+uy**2), 0, 0.4)
+            print(v.mean(), v.max(), v.std(), v.min())
+            plt.imshow(v)
             plt.pause(0.01)
             plt.cla()
 
